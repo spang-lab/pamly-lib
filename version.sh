1ea9d977e3362bb3e4e619a5901933b25fa13f8d
Michael Huttner <michael@mhuttner.com>
2023-11-22 12:22:59 +0100

    version script

---
+++ version.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+  echo "usage: version.sh <version>"
+  echo "This will update the version for all packages"
+  exit 1
+fi
+
+if command -v gsed >/dev/null 2>&1; then
+    SED_COMMAND="gsed"
+else
+    SED_COMMAND="sed"
+fi
+
+
+
+VERSION="$(npm version --no-git-tag-version "$1")"
+$SED_COMMAND -i "s/^version = .*/version = \"${VERSION#v}\"/" Cargo.toml
+
+npm install  # update lockfile
+git add -A
+git commit --message "$VERSION"
+git tag -a -m "New version: $VERSION" "$VERSION"
+
+
+
+
